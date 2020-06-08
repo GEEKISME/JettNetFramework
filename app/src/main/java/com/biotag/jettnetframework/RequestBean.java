0f53fe69c3b8a52f3e5e0b12025b9418c4444a28
liandfeng <lixiaohui124213@sina.com>
2020-06-08 16:54:21 +0800

    完善

---
+++ app/src/main/java/com/biotag/jettnetframework/RequestBean.java
@@ -0,0 +1,22 @@
+package com.biotag.jettnetframework;
+
+public class RequestBean {
+    private String provide_id;
+    private String key;
+
+    public String getProvide_id() {
+        return provide_id;
+    }
+
+    public void setProvide_id(String provide_id) {
+        this.provide_id = provide_id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
